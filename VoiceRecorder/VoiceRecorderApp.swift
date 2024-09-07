eba9f8c55d6ae71e240a2b5ca73621967d9d5293
george <george@georges-MacBook-Air.local>
2024-09-07 15:37:23 +0700

    init swiftUI project

---
+++ VoiceRecorder/VoiceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderApp.swift
+//  VoiceRecorder
+//
+//  Created by george on 07.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
