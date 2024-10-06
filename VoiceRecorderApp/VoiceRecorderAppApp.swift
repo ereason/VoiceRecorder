52436b86c9c7f6864099964828511382032646d6
george <george@georges-MacBook-Air.local>
2024-10-06 22:59:56 +0700

    Init new project

---
+++ VoiceRecorderApp/VoiceRecorderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderAppApp.swift
+//  VoiceRecorderApp
+//
+//  Created by george on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecorderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
